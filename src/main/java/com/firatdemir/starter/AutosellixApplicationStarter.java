163ffaac996c4fdb5025c830495ecb74808d38ee
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-05-12 20:52:39 +0300

    Yeni entity sınıfları eklendi

--- src/main/java/com/firatdemir/starter/AutosellixApplicationStarter.java
+++ src/main/java/com/firatdemir/starter/AutosellixApplicationStarter.java
@@ -2,7 +2,13 @@ package com.firatdemir.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan(basePackages = { "com.firatdemir" })
+@EntityScan(basePackages = { "com.firatdemir" })
+@EnableJpaRepositories(basePackages = { "com.firatdemir" })
 @SpringBootApplication
 public class AutosellixApplicationStarter {
 
