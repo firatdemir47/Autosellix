a968303b42a678360bd879c46d6a42c179cfbb99
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-05-13 10:02:33 +0300

    Exception Mimarisi Kuruldu

---
+++ src/main/java/com/firatdemir/service/IAddressService.java
@@ -0,0 +1,5 @@
+package com.firatdemir.service;
+
+public interface IAddressService {
+
+}
---
+++ src/main/java/com/firatdemir/service/impl/AddressServiceImpl.java
@@ -0,0 +1,10 @@
+package com.firatdemir.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.firatdemir.service.IAddressService;
+
+@Service
+public class AddressServiceImpl implements IAddressService {
+
+}
