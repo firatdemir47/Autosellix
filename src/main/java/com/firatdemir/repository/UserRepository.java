982f69ae89876057b1ba375aad1279a4d84259b4
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-05-16 11:36:48 +0300

    AppConfig sınıfı oluşturuldu ve güvenlik yapılandırmaları eklendi

---
+++ src/main/java/com/firatdemir/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.firatdemir.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.firatdemir.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByUsername(String username);
+}
