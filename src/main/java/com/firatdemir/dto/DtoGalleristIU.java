c2cf13c6b4d980b1d34428dc5891a5fb08a3b1fc
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-05-28 10:07:14 +0300

    feat: Gallerist ekleme/güncelleme işlemleri için DtoGalleristIU sınıfı eklendi

---
+++ src/main/java/com/firatdemir/dto/DtoGalleristIU.java
@@ -0,0 +1,19 @@
+package com.firatdemir.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DtoGalleristIU {
+
+	private String firstName;
+
+	private String lastName;
+
+	private Long addressID;
+}
