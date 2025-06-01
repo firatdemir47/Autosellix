d442bed14c194ddee45de75cae832ba8d45743fe
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-06-01 11:34:23 +0300

    feat: Gallerist ve araç ilişkisini kaydeden IGalleristCarService arayüzü eklendi

---
+++ src/main/java/com/firatdemir/service/IGalleristCarService.java
@@ -0,0 +1,9 @@
+package com.firatdemir.service;
+
+import com.firatdemir.dto.DtoGalleristCar;
+import com.firatdemir.dto.DtoGalleristIU;
+
+public interface IGalleristCarService {
+
+	public DtoGalleristCar saveGalleristCa(DtoGalleristIU dtoGalleristIU);
+}
