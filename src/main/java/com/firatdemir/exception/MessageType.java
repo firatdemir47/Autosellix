690f587251bab9adb9c734e513d37bcf57e15f5b
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-06-05 13:14:35 +0300

    feat: TCMB döviz kuru verilerini çekmek için CurrencyRatesService implementasyonu eklendi

--- src/main/java/com/firatdemir/exception/MessageType.java
+++ src/main/java/com/firatdemir/exception/MessageType.java
@@ -7,7 +7,9 @@ public enum MessageType {
 	NO_RECORD_EXIST("1004", "kayıt bulunamadı"), TOKEN_IS_EXPIRED("1005", "tokenin süresi bitmiştir "),
 	USERNANE_NOT_FOUND("1006", "username bulunamadı"),
 	USERNAME_OR_PASSWORD_INVALID("1007", "kullanıcı adı veya şifre hatalı"),
-	REFRESH_TOKEN_NOT_FOUND("1008", "refresh token bulunamadı"),REFRESH_TOKEN_IS_EXPIRED("1009","refres tokenın süresi bitmiştir"), GENERAL_EXCEPTION("9999", "genel bir hata oluştu");
+	REFRESH_TOKEN_NOT_FOUND("1008", "refresh token bulunamadı"),
+	REFRESH_TOKEN_IS_EXPIRED("1009", "refres tokenın süresi bitmiştir"),
+	CURRENY_RATES_IS_OCCURED("1010", "döviz kuru alınamadı"), GENERAL_EXCEPTION("9999", "genel bir hata oluştu");
 
 	private String code;
 
